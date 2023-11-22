7c4e3c5271cbed81a7ac8110316309fbbf575c0e
Ramkumar96 <sivaram0203@gmail.com>
2023-11-22 12:33:23 -0500

    Implementation of Screens and Main activity and navigations

--- app/src/main/java/com/example/movieflix/IntroActivity.java
+++ app/src/main/java/com/example/movieflix/IntroActivity.java
@@ -3,8 +3,11 @@
 package com.example.movieflix;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class IntroActivity extends AppCompatActivity {
 
@@ -12,5 +15,17 @@ public class IntroActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intro);
+
+        AppCompatButton getStartedButton = findViewById(R.id.getStartedBtn);
+
+        getStartedButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Create an Intent to start the MainActivity
+                Intent intent = new Intent(IntroActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/movieflix/MainActivity.java
+++ app/src/main/java/com/example/movieflix/MainActivity.java
@@ -2,13 +2,26 @@ package com.example.movieflix;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
 
+    FloatingActionButton floatingActionButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        floatingActionButton = (FloatingActionButton)findViewById(R.id.floatingactionbutton);
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), AddMovieActivity.class));
+            }
+        });
     }
 }
