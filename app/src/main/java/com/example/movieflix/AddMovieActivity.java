e0531cc0cad023ce352bd3bb313aa62b40d32fc6
Pinto-PINTO <marie334fe@gmail.com>
2023-11-23 18:48:39 -0500

    Made Add Movie and Edit Movie UIs

--- app/src/main/java/com/example/movieflix/AddMovieActivity.java
+++ app/src/main/java/com/example/movieflix/AddMovieActivity.java
@@ -2,7 +2,12 @@ package com.example.movieflix;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class AddMovieActivity extends AppCompatActivity {
 
@@ -10,5 +15,19 @@ public class AddMovieActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_movie);
+
+
+        //toolbar back button
+        ImageView backButton = findViewById(R.id.toolbar_back_icon);
+        TextView toolbarText = findViewById(R.id.toolbar_text);
+        toolbarText.setText("Add Movie");
+
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(AddMovieActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/movieflix/EditMovieActivity.java
+++ app/src/main/java/com/example/movieflix/EditMovieActivity.java
@@ -2,7 +2,11 @@ package com.example.movieflix;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class EditMovieActivity extends AppCompatActivity {
 
@@ -10,5 +14,18 @@ public class EditMovieActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_movie);
+
+        //toolbar back button
+        ImageView backButton = findViewById(R.id.toolbar_back_icon);
+        TextView toolbarText = findViewById(R.id.toolbar_text);
+        toolbarText.setText("Edit Movie");
+
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(EditMovieActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
